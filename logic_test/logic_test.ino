ac90108fb5ab3fad1a85eb94038ddf130e39a274
minsoo3102 <159438834+minsoo3102@users.noreply.github.com>
2024-09-06 12:55:40 +0900

    Add files via upload

---
+++ logic_test/logic_test.ino
@@ -0,0 +1,12 @@
+#define pin_tset 3 //test pin
+
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(pin_test, output);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  
+}
